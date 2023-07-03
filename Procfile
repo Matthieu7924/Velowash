e90b1f8a4696c8e5b1ab3775ff2fcbb48340b2e1
matt <matthieugarban@gmail.com>
2023-07-03 11:03:34 +0200

    adding vars 4 app-env et _adatabase_url

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: php -S 0.0.0.0:$PORT
+web: heroku-php-apache2 index.php
+
+
--- connect.php
+++ connect.php
@@ -1,4 +1,13 @@
 <?php
-$bdd = new PDO('sqlite:SRC/database/chat.db');
-$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$env = getenv('APP_ENV') ?: 'dev';
+
+if ($env === 'prod') {
+    // Configuration pour l'environnement de production
+    $databaseUrl = getenv('DATABASE_URL');
+    // Utilisez la variable $databaseUrl pour la connexion à votre base de données
+}
+else {
+    $bdd = new PDO('sqlite:SRC/database/chat.db');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+}
