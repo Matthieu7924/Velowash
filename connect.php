42631756faa761af140c64303275000e2a86f731
matt <matthieugarban@gmail.com>
2023-07-18 14:50:59 +0200

    env prod in connect.php

--- connect.php
+++ connect.php
@@ -3,17 +3,17 @@ $env = getenv('APP_ENV') ?: 'dev';
 
 if ($env === 'prod') {
     // Configuration pour l'environnement de production
-    $databaseUrl = getenv('DATABASE_URL');
-    // $databaseUrl = getenv('CLEARDB_DATABASE_URL');
-    // $url = parse_url($databaseUrl);
+    // $databaseUrl = getenv('DATABASE_URL');
+    $databaseUrl = getenv('CLEARDB_DATABASE_URL');
+    $url = parse_url($databaseUrl);
 
-    // $bdd = new PDO(
-    //     'mysql:host=' . $url['host'] . ';dbname=' . substr($url['path'], 1),
-    //     $url['user'],
-    //     $url['pass']
-    // );
-    // $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $bdd = new PDO(
+        'mysql:host=' . $url['host'] . ';dbname=' . substr($url['path'], 1),
+        $url['user'],
+        $url['pass']
+    );
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
     // Utilisez la variable $databaseUrl pour la connexion à votre base de données
 }
