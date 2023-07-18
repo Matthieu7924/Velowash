46fc5eb19327e24d770f23bb1d3dfb6da86d5a67
matt <matthieugarban@gmail.com>
2023-07-18 14:56:14 +0200

    modif index.php by adding var

--- index.php
+++ index.php
@@ -21,6 +21,30 @@ use \Messenger\RoomsManager;
 use \Messenger\CategoryManager;
 use \Messenger\MessagesManager;
 
+$env = getenv('APP_ENV') ?: 'dev';
+
+if ($env === 'prod') {
+    // Configuration pour l'environnement de production
+    $databaseUrl = getenv('CLEARDB_DATABASE_URL');
+    $url = parse_url($databaseUrl);
+
+    $bdd = new PDO(
+        'mysql:host=' . $url['host'] . ';dbname=' . substr($url['path'], 1),
+        $url['user'],
+        $url['pass']
+    );
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    // Utilisez la variable $bdd pour la connexion à votre base de données
+} else {
+    // Configuration pour l'environnement de développement local
+    $bdd = new PDO('sqlite:SRC/database/chat.db');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+}
+
+
 $catManager = new CategoryManager($bdd);
 $roomManager = new RoomsManager($bdd);
 $messManager = new MessagesManager($bdd);
