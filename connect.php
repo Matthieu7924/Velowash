13ae3f4bd3ce8668081f47bb8a5f92d140dd5109
matt <matthieugarban@gmail.com>
2023-07-18 19:11:10 +0200

    modif  4 connect.php

--- connect.php
+++ connect.php
@@ -20,6 +20,7 @@ if ($env === 'prod') {
 else {
     // Configuration pour l'environnement de développement local
     $bdd = new PDO('sqlite:SRC/database/chat.db');
+
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 }
